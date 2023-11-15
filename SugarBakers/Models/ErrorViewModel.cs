cd88e3fd001c2f47d434fa95f06d543aab9a7d26
simpyb761 <simpyb761@gmail.com>
2023-11-15 18:22:38 -0500

    Completed Program v2

---
+++ SugarBakers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SugarBakers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
